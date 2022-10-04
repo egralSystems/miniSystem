8f881fee47b170df5637e4c13b0b9d4346a0f3ed
m1cha1s <msulej04@gmail.com>
2022-10-04 23:13:23 +0200

    Switched to WREN

--- src/core/miniSystem.cpp
+++ src/core/miniSystem.cpp
@@ -2,10 +2,9 @@
 
 #include "miniSystem.hpp"
 
-MiniSystem::MiniSystem(duk_fatal_function fatal_handler, duk_alloc_function alloc_func, duk_realloc_function realloc_func, duk_free_function free_func, void *heap_udata)
+MiniSystem::MiniSystem()
 {
-    ctx = duk_create_heap(alloc_func, realloc_func, free_func, heap_udata, fatal_handler);
-    init_console(ctx);
+    jerry_init(JERRY_INIT_EMPTY);
 }
 
 MiniSystem::~MiniSystem()
--- src/core/miniSystem.hpp
+++ src/core/miniSystem.hpp
@@ -1,15 +1,12 @@
 #pragma once
-#include <duktape.h>
+#include <jerryscript.h>
 
 #include <console.hpp>
 
 class MiniSystem
 {
-private:
-    duk_context *ctx;
-
 public:
-    MiniSystem(duk_fatal_function fatal_handler = NULL, duk_alloc_function alloc_func = NULL, duk_realloc_function realloc_func = NULL, duk_free_function free_func = NULL, void *heap_udata = NULL);
+    MiniSystem();
     ~MiniSystem();
 
     int eval(const char *);
