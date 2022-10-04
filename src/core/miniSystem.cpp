a3398a837c00c465381d94afa151cec3c54601f5
m1cha1s <msulej04@gmail.com>
2022-10-05 00:30:29 +0200

    It works again.

--- src/core/miniSystem.cpp
+++ src/core/miniSystem.cpp
@@ -4,6 +4,8 @@
 
 #include "miniSystem.hpp"
 
+Stream<std::string> consoleStream;
+
 MiniSystem::MiniSystem(StreamIf<std::string> *consoleStreamIf)
 {
     consoleStream.init(consoleStreamIf);
@@ -18,32 +20,6 @@ MiniSystem::MiniSystem(StreamIf<std::string> *consoleStreamIf)
 MiniSystem::~MiniSystem()
 {
     wrenFreeVM(vm);
-}
-
-void MiniSystem::writeFn(WrenVM *vm, const char *msg)
-{
-    consoleStream << msg;
-}
-void MiniSystem::errorFn(WrenVM *vm, WrenErrorType errorType, const char *module, const int line, const char *msg)
-{
-    switch (errorType)
-    {
-    case WREN_ERROR_COMPILE:
-    {
-        consoleStream << "[" << std::to_string(line) << "] [Error] " << msg << "\n";
-    }
-    break;
-    case WREN_ERROR_STACK_TRACE:
-    {
-        consoleStream << "[" << module << " line " << std::to_string(line) << "] in " << msg << "\n";
-    }
-    break;
-    case WREN_ERROR_RUNTIME:
-    {
-        consoleStream << "[Runtime Error] " << msg << "\n";
-    }
-    break;
-    }
 }
 
 int MiniSystem::eval(const char *prog)
--- src/core/miniSystem.hpp
+++ src/core/miniSystem.hpp
@@ -2,18 +2,43 @@
 #include <wren.hpp>
 #include <stream.hpp>
 
+extern Stream<std::string> consoleStream;
+
 class MiniSystem
 {
 private:
     WrenVM *vm;
-    static Stream<std::string> consoleStream;
 
 public:
     MiniSystem(StreamIf<std::string> *consoleStreamIf);
     ~MiniSystem();
 
-    static void writeFn(WrenVM *, const char *);
-    static void errorFn(WrenVM *, WrenErrorType, const char *, const int, const char *);
+    static void writeFn(WrenVM *vm, const char *msg)
+    {
+        consoleStream << msg;
+    }
+
+    static void errorFn(WrenVM *vm, WrenErrorType errorType, const char *module, const int line, const char *msg)
+    {
+        switch (errorType)
+        {
+        case WREN_ERROR_COMPILE:
+        {
+            consoleStream << "[" << std::to_string(line) << "] [Error] " << msg << "\n";
+        }
+        break;
+        case WREN_ERROR_STACK_TRACE:
+        {
+            consoleStream << "[" << module << " line " << std::to_string(line) << "] in " << msg << "\n";
+        }
+        break;
+        case WREN_ERROR_RUNTIME:
+        {
+            consoleStream << "[Runtime Error] " << msg << "\n";
+        }
+        break;
+        }
+    }
 
     int
     eval(const char *);
